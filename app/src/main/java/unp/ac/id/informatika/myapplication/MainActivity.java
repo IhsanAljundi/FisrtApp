ebfd8dae35f34670bbe75d4bfcd5761c1c039d3c
afifsasonda <afifdwisasonda286@gmail.com>
2022-09-22 08:05:18 +0700

    first init

---
+++ app/src/main/java/unp/ac/id/informatika/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package unp.ac.id.informatika.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
